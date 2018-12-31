893dec3df42f50d64ffc86c89a1e148716db9065
04gas90@mail.ru <04gas90@mail.ru>
2018-12-31 16:18:25 +0400

    completed

--- src/main/java/ru/gas/filesstructured/Main.java
+++ src/main/java/ru/gas/filesstructured/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(
                 "Программа структурирования файлов по подпапкам с именем [год]_[номер месяца]\n" +
-                "Год и номер месяца - берутся из даты изменения файла.\n" +
+                "Год и номер месяца - берутся из даты съемки если это фото, иначе из даты изменения файла.\n" +
                 "Путь к корневой директории указать в аргументах программы.\n");
         if(args.length == 0)
             System.out.println("Введите в аргументах адрес корневой директории\n");
         else {
-            MovingFile fr = new MovingFile(args[0]);
-            fr.renameFolders();
+            MovingFile movingFile = new MovingFile(args[0]);
+            movingFile.run();
         }
     }
 }
--- src/main/java/ru/gas/filesstructured/MovingFile.java
+++ src/main/java/ru/gas/filesstructured/MovingFile.java
@@ -1,9 +1,16 @@
 package ru.gas.filesstructured;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.time.*;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 
 public class MovingFile {
     private File directory;
@@ -16,40 +23,66 @@ public class MovingFile {
             throw new IllegalArgumentException("Необходимо указать директорию");
     }
 
-    public void renameFolders() {
-        File[] innerDirectories = directory.listFiles();
-        if (innerDirectories != null) {
-            for (File innerDirectory : innerDirectories) {
-                String oldFullName = innerDirectory.getAbsolutePath();
-                String oldName = innerDirectory.getName();
-                String mp4FIleName = getMP4FileName(innerDirectory);
-                if (mp4FIleName == null) {
-                    System.out.printf("\"%s\" - в данной папке нет MP4 файлов, начинающихся с цифр и тире%n", oldName);
-                } else {
-                    String newName =
-                            oldFullName.substring(
-                                    0, oldFullName.lastIndexOf(oldName)
-                            ) + mp4FIleName;
-                    //выполняем переименование и проверяем успешно оно или нет
-                    if (innerDirectory.renameTo(new File(newName)))
-                        System.out.printf("\"%s\" - успешно переименовано в \"%s\"%n", oldName, mp4FIleName);
-                    else
-                        System.out.printf("\"%s\" - ошибка переименования в \"%s\"%n", oldName, mp4FIleName);
+    public void run() {
+        Iterator<File> iter = FileUtils.iterateFiles(directory, null, false);
+        List<String> errors = new ArrayList<>();
+        if (iter.hasNext()) {
+            File f;
+            long i = 0;
+            while (iter.hasNext()) {
+                i++;
+                f = iter.next();
 
+                LocalDateTime date = null;
+                String log = null;
+                try {
+                    Metadata metadata = ImageMetadataReader.readMetadata(f);
+                    ExifSubIFDDirectory exif = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+                    if (exif != null) {
+                        date = convertMillisToLocalDateUTC(exif.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL).getTime());
+                        log = String.format("%s: %s_%s_%s_%sh", f.getName(), date.getYear(), date.getMonthValue(), date.getDayOfMonth(), date.getHour());
+                    }
+                } catch (Exception ignored) {}
+                if (date == null) {
+                    date = convertMillisToLocalDate(f.lastModified());
+                    log = String.format("It doesn't have metadata (%s): %s_%s_%s_%sh", f.getName(), date.getYear(), date.getMonthValue(), date.getDayOfMonth(), date.getHour());
+                }
+
+                String dirName = "NOT LOAD NAME";
+                try {
+                    dirName = String.format("%s\\%s_%s", directory, date.getYear(), date.getMonthValue());
+                    File dir = new File(dirName);
+                    if (!dir.exists()) {
+                        if (dir.mkdir()) {
+                            System.out.println(String.format("Created directory: %s", dirName));
+                        } else {
+                            throw new RuntimeException("Failed to create directory!");
+                        }
+                    }
+                    FileUtils.moveFileToDirectory(f, dir, false);
+                    System.out.println(String.format("\t%d Success moved - %s", i, log));
+                } catch (Exception e) {
+                    String errText = String.format("\tError by moving %s to: %s (%s)", f.getName(), dirName, e.getMessage());
+                    System.out.println(errText);
+                    errors.add(errText);
                 }
             }
-        } else
-            throw new IllegalArgumentException(directory.getAbsolutePath() + " - укзанная директория пуста");
+        } else {
+            String errText = directory.getAbsolutePath() + " - this directory is empty";
+            errors.add(errText);
+        }
+        System.out.printf("Process completed with %d errors.\n", errors.size());
+        for (int i = 0; i < errors.size(); i++) {
+            System.out.printf("Error %d : %s\n", i+1, errors.get(i));
+        }
     }
 
-    private String getMP4FileName(File innerDirectory) {
-        Iterator<File> iter = FileUtils.iterateFiles(innerDirectory, new String[]{"mp4"}, false);
-        File f;
-        if (iter.hasNext()) {
-            f = iter.next();
-            if (f.getName().matches("^\\d+-.*"))//если файл начинается с цифры и тире
-                return f.getName().replace(".mp4", "");
-        }
-        return null;
+    private LocalDateTime convertMillisToLocalDate(Long l) {
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(l), ZoneId.systemDefault());
     }
+
+    private LocalDateTime convertMillisToLocalDateUTC(Long l) {
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(l), ZoneOffset.UTC);
+    }
+
 }
